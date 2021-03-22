cc56dbdc620bc2aca40f6f69a2b732098e0986bd
Jonas Rittershofer <jotoeri@users.noreply.github.com>
2021-03-22 23:09:01 +0100

    New Approach
    Signed-off-by: Jonas Rittershofer <jotoeri@users.noreply.github.com>

--- translations/translationtool/src/translationtool.php
+++ translations/translationtool/src/translationtool.php
@@ -326,51 +326,71 @@ class TranslatableApp {
 				continue;
 			}
 
-			// t - Single Translations
-			$tRegexArray = [
-				"/\Wt\s*\(\s*'([\w]+)',\s*'(.+)'/", // Single Quotes
-				"/\Wt\s*\(\s*\"([\w]+)\",\s*\"(.+)\"/", // Double Quotes
-				"/\Wt\s*\(\s*\'([\w]+)\'\s*,\s*\`(.+)\`\s*\)/msU" // Template Quotes
+			// Optional TRANSLATORS-comment
+			$translatorsRegex = "(\W(\/\/|\/\*|<!--)\s(TRANSLATORS)\s(.+))*";
+			// Text between comment and translation-function
+			$inbetweenRegex = "(\s*.*\s*)";
+
+			/**
+			 * t - Singular Translations
+			 */
+			// Regex for translation-function t. Items Contain Regex-Ending '/' as template quotes needs special ending
+			$tTranslationRegex = [
+				"\Wt\s*\(\s*'([\w]+)',\s*'(.+)'" . "/",					// Single Quotes
+				"\Wt\s*\(\s*\"([\w]+)\",\s*\"(.+)\"" . "/",				// Double Quotes
+				"\Wt\s*\(\s*\'([\w]+)\'\s*,\s*\`(.+)\`\s*\)" . "/msU"	// Template Quotes
 			];
-			foreach($tRegexArray as $regex) {
-				$regexMatches = preg_grep($regex, $vueSource);
+			foreach ($tTranslationRegex as $tRegex) {
+				// Regex-Ending within tRegex, see array above.
+				preg_match_all("/". $translatorsRegex . $inbetweenRegex . $tRegex, $vueSource, $matches);
 
-				foreach ($regexMatches as $lineNum => $match) {
-					// Check for comment in previous line. Either HTML or JS comment.
-					if(preg_match("/\W(\/\/|\/\*|<!--)\s(TRANSLATORS)\s(.+)/", $vueSource[$lineNum-1], $commentSplit)) {
-						// Remove possible comment-end-tags and insert comment
-						$commentText = preg_replace('/\s*(-->|\*\/)\s*/', '', $commentSplit[3]);
-						$fakeFileContent .= "// TRANSLATORS " . preg_replace('/\s+/', ' ', $commentText) . PHP_EOL;
+				/* The variable $matches now contains 8 Arrays corresponding to the Regex-Groups, out of which:
+				* $matches[0] Holds an array of the whole Matches
+				* $matches[4] Holds an array of the comments, but still including a possible comment-ending (-->|* /)
+				* $matches[7] Holds an array of the texts to translate
+				* All inner arrays contain the respective content with corresponding indices.
+				* So $matches[4][2] is the corresponding comment to the text in $matches[7][2]
+				*/
+				foreach ($matches[0] as $index => $fullMatch) {
+					if ($matches[4][$index] !== '') {
+						// Replace end-tags by nothing
+						$comment = preg_replace('/\s*(-->|\*\/)\s*/', '', $matches[4][$index]);
+						$fakeFileContent .= "// TRANSLATORS " . $comment . PHP_EOL;
 					}
-
-					// Insert translation-function
-					preg_match($regex, $match, $matchSplit);
-					$fakeFileContent .= "t('" . $this->name . "', '" . preg_replace('/\s+/', ' ', $matchSplit[2]) . "');" . PHP_EOL;
+					$fakeFileContent .= "t('" . $this->name . "', '" . preg_replace('/\s+/', ' ', $matches[7][$index]) . "');" . PHP_EOL;
 				}
-			}
+			};
 
-			// n - Plural Translations
-			$nRegexArray = [
-				"/\Wn\s*\(\s*'([\w]+)',\s*'(.+)'\s*,\s*'(.+)'\s*(.+)/", // Single Quotes
-				"/\Wn\s*\(\s*\"([\w]+)\",\s*\"(.+)\"\s*,\s*\"(.+)\"\s*(.+)/", // Double Quotes
-				"/\Wn\s*\(\s*\'([\w]+)\'\s*,\s*\`(.+)\`\s*,\s*\`(.+)\`\s*\)/msU" // Template Quotes
+			/**
+			 * n - Plural translations
+			 */
+			// Regex for translation-function n. Items Contain Regex-Ending '/' as template quotes needs special ending
+			$nTranslationRegex = [
+				"\Wn\s*\(\s*'([\w]+)',\s*'(.+)'\s*,\s*'(.+)'\s*(.+)" . "/",				// Single Quotes
+				"\Wn\s*\(\s*\"([\w]+)\",\s*\"(.+)\"\s*,\s*\"(.+)\"\s*(.+)" . "/",		// Double Quotes
+				"\Wn\s*\(\s*\'([\w]+)\'\s*,\s*\`(.+)\`\s*,\s*\`(.+)\`\s*\)" . "/msU"	// Template Quotes
 			];
-			foreach($nRegexArray as $regex) {
-				$regexMatches = preg_grep($regex, $vueSource);
+			foreach ($nTranslationRegex as $nRegex) {
+				// Regex-Ending within tRegex, see array above.
+				preg_match_all("/". $translatorsRegex . $inbetweenRegex . $nRegex, $vueSource, $matches);
 
-				foreach ($regexMatches as $lineNum => $match) {
-					// Check for comment in previous line. Either HTML or JS comment.
-					if(preg_match("/\W(\/\/|\/\*|<!--)\s(TRANSLATORS)\s(.+)/", $vueSource[$lineNum-1], $commentSplit)) {
-						// Remove possible comment-end-tags and insert comment
-						$commentText = preg_replace('/\s*(-->|\*\/)\s*/', '', $commentSplit[3]);
-						$fakeFileContent .= "// TRANSLATORS " . preg_replace('/\s+/', ' ', $commentText) . PHP_EOL;
+				/* The variable $matches now contains 8 Arrays corresponding to the Regex-Groups, out of which:
+				* $matches[0] Holds an array of the whole Matches
+				* $matches[4] Holds an array of the comments, but still including a possible comment-ending (-->|* /)
+				* $matches[7] Holds an array of the singular form of the text to translate
+				* $matches[8] Holds an array of the plural form of the text to translate
+				* All inner arrays contain the respective content with corresponding indices.
+				* So $matches[4][2] is the corresponding comment to the text in $matches[7][2]
+				*/
+				foreach ($matches[0] as $index => $fullMatch) {
+					if ($matches[4][$index] !== '') {
+						// Replace end-tags by nothing
+						$comment = preg_replace('/\s*(-->|\*\/)\s*/', '', $matches[4][$index]);
+						$fakeFileContent .= "// TRANSLATORS " . $comment . PHP_EOL;
 					}
-
-					// Insert translation-function
-					preg_match($regex, $match, $matchSplit);
-					$fakeFileContent .= "n('" . $this->name . "', '" . preg_replace('/\s+/', ' ', $matchSplit[2]) . "', '" . preg_replace('/\s+/', ' ', $matchSplit[3]) . "');" . PHP_EOL;
+					$fakeFileContent .= "n('" . $this->name . "', '" . preg_replace('/\s+/', ' ', $matches[7][$index]) . "', '" . preg_replace('/\s+/', ' ', $matches[8][$index]) . "');" . PHP_EOL;
 				}
-			}
+			};
 		}
 
 		file_put_contents($this->fakeVueFile, $fakeFileContent);
